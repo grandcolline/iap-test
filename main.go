99875b9e6bab80c1e39e62b394a64046290882f9
Taiki Hayashi <grandcolline@gmail.com>
2019-12-07 16:25:19 +0900

    format

--- main.go
+++ main.go
@@ -7,13 +7,11 @@ import (
 
 func main() {
 	fmt.Println("server start...")
-    http.HandleFunc("/hello", helloHandler)
-    http.ListenAndServe(":8080", nil)
+	http.HandleFunc("/hello", helloHandler)
+	http.ListenAndServe(":8080", nil)
 }
 
-// リクエストを処理する関数
 func helloHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("hello handler")
-    fmt.Fprint(w, "Hello World")
+	fmt.Fprint(w, "Hello World")
 }
-
