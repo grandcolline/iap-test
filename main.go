67fbda0b24cd9827e6b7a9c097a15f6201f5d629
Taiki Hayashi <grandcolline@gmail.com>
2019-12-11 07:57:24 +0900

    add userhandler

--- main.go
+++ main.go
@@ -8,10 +8,17 @@ import (
 func main() {
 	fmt.Println("server start...")
 	http.HandleFunc("/hello", helloHandler)
+	http.HandleFunc("/user", userHandler)
 	http.ListenAndServe(":8080", nil)
 }
 
 func helloHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("hello handler")
 	fmt.Fprint(w, "Hello World")
+}
+
+func userHandler(w http.ResponseWriter, r *http.Request) {
+	mail := r.Header.Get("X-Goog-Authenticated-User-Email")
+	id := r.Header.Get("X-Goog-Authenticated-User-ID")
+	fmt.Fprint(w, "ID: "+id+"\nmail: "+mail)
 }
