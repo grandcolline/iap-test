95d26f976f30d2d4d57c33555160c106462bdc42
Taiki Hayashi <grandcolline@gmail.com>
2019-12-12 14:26:26 +0900

    add tokenHandler

--- main.go
+++ main.go
@@ -16,6 +16,7 @@ func main() {
 	http.HandleFunc("/hello", helloHandler)
 	http.HandleFunc("/user", userHandler)
 	http.HandleFunc("/auth", authHandler)
+	http.HandleFunc("/token", tokenHandler)
 	http.ListenAndServe(":8080", nil)
 }
 
@@ -92,3 +93,42 @@ func fetchPublicKey(keyID string) ([]byte, error) {
 	}
 	return []byte(skeys[keyID]), nil
 }
+
+// tokenHandler
+func tokenHandler(w http.ResponseWriter, r *http.Request) {
+	// get token
+	tokenStr := r.Header.Get("X-Goog-IAP-JWT-Assertion")
+	audience := os.Getenv("AUDIENCE")
+	claim := &iapClaim{}
+
+	_, err := jwt.ParseWithClaims(tokenStr, claim, func(token *jwt.Token) (interface{}, error) {
+		// check signing method
+		if _, ok := token.Method.(*jwt.SigningMethodECDSA); !ok {
+			return nil, errors.New("Unexpected signing method")
+		}
+		// get public key
+		if _, ok := token.Header["kid"].(string); !ok {
+			return nil, errors.New("not found kid")
+		}
+		rawKey, err := fetchPublicKey(token.Header["kid"].(string))
+		if err != nil {
+			return nil, err
+		}
+		key, err := jwt.ParseECPublicKeyFromPEM(rawKey)
+		if err != nil {
+			return nil, err
+		}
+		return key, nil
+	})
+	if err != nil {
+		fmt.Fprint(w, "Validation: NG"+"\nerror: "+err.Error())
+		return
+	}
+
+	if claim.Audience == audience {
+		fmt.Fprint(w, "Validation: OK"+"\nID: "+claim.Subject+"\nmail: "+claim.Email+"\ntoken: "+tokenStr)
+		return
+	}
+	fmt.Fprint(w, "Validation: NG"+"\nerror: invalid audience")
+}
+
